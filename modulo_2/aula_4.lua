03b85af3664c8920c5e540b541b1d665fab9d034
Andrade1221 <111130558+Andrade1221@users.noreply.github.com>
2025-03-21 10:01:19 -0300

    Correção aula 20/03/2025

--- modulo_2/aula_4.lua
+++ modulo_2/aula_4.lua
@@ -1,10 +1,10 @@
 x = 10
 y = 20
 
-print(x+y) -- Adição
-print(x-y) -- Subtração
-print(x*y) -- Multiplicação
-print(x/y) -- Divisão
-print(x^y) -- Potencia
-print(10+30) -- Pode ser direto
-print(1/10^20) -- Raiz Quadrada
+print(x+y) -- Adição (10 + 20 = 30)
+print(x-y) -- Subtração (10 - 20 = -10)
+print(x*y) -- Multiplicação (10 * 20 = 200)
+print(x/y) -- Divisão (10 / 20 = 0.5)
+print(x^y) -- Potência (10^20 = 10 elevado a 20)
+print(10+30) -- Pode ser feito diretamente (10 + 30 = 40)
+print(10^(1/2)) -- Raiz quadrada de 10
