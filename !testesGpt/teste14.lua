7eca2a1c02d6470d3cca2f031f22ce210738c456
Andrade1221 <111130558+Andrade1221@users.noreply.github.com>
2025-05-13 10:53:30 -0300

    Top

--- !testesGpt/teste14.lua
+++ !testesGpt/teste14.lua
@@ -23,3 +23,60 @@ filterByMinAge(21)
 sortUsersByName()
 ```
 ]]
+
+local users = {
+    { name = 'Gabriel', age = 19 },
+    { name = 'Lucca', age = 22 },
+    { name = 'Mario', age = 35 },
+    { name = 'Amanda', age = 7 },
+    { name = 'Bruna', age = 64 }
+}
+
+function showUsers(users)
+    for i, user in ipairs(users) do
+        print('Usuario [' .. i .. ']: ' .. user.name .. ', ' .. user.age .. ' anos')
+    end
+end
+
+function removeUser(name)
+    for i, user in ipairs(users) do
+        if user.name == name then
+            table.remove(users, i)
+            return
+        end
+    end
+    print('Usuario: ' .. name .. ' nao encontrado.')
+end
+
+function filterByMinAge(age)
+    local names = 'Lista:\n'
+    for i, user in ipairs(users) do
+        if user.age >= age then
+            names = names .. 'Usuario [' .. i .. ']: ' .. user.name .. ', ' .. user.age .. ' anos\n'
+        end
+    end
+    
+    if names == 'Lista:\n' then
+        print('Nenhum usuario com essa idade.')
+    else
+        print(names)
+    end
+end
+
+function sortUsersByName()
+    table.sort(users, function(a, b)
+        return a.name < b.name
+    end)
+end
+
+showUsers(users)
+
+sortUsersByName()
+
+filterByMinAge(10)
+filterByMinAge(100)
+
+removeUser('Lucca')
+removeUser('Luca')
+
+showUsers(users)
